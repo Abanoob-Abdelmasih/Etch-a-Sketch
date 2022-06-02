3db50d43c6fbed7b3ddc642ec410ef4ef4607e5f
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-02 14:40:45 +0300

    refactored the codes in all 3 files

--- script.js
+++ script.js
@@ -2,21 +2,19 @@
 
 const container = document.querySelector('.container');
 
-function changeColor(e) {
-  console.log(e);
-  if (e.target.classList.contains('gridItem')) {
-    e.target.classList.add('hovered');
-  }
-}
-
 for (let i = 0; i < 256; i++) {
   const div = document.createElement('div');
   div.classList.add('gridItem');
   container.appendChild(div);
 }
 
+function changeColor(e) {
+  if (e.target.classList.contains('gridItem')) {
+    e.target.classList.add('hovered');
+  }
+}
+
 const divs = document.querySelectorAll('div');
-console.log(divs);
 
 divs.forEach((div) => {
   div.addEventListener('mouseenter', changeColor);
