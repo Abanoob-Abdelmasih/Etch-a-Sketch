a6ad4bce46a4cf459cb7ca07c27fce4e010057ed
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-02 14:45:57 +0300

    added prompt popup to the button

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const container = document.querySelector('.container');
+const customInput = document.querySelector('.custom-size-input');
 
 for (let i = 0; i < 256; i++) {
   const div = document.createElement('div');
   div.classList.add('gridItem');
   container.appendChild(div);
 }
+
+// /////////////////////////////////////////////////////////////////////////
 
 function changeColor(e) {
   if (e.target.classList.contains('gridItem')) {
@@ -18,4 +21,10 @@ const divs = document.querySelectorAll('div');
 
 divs.forEach((div) => {
   div.addEventListener('mouseenter', changeColor);
+});
+
+// /////////////////////////////////////////////////////////////////////////
+
+customInput.addEventListener('click', () => {
+  prompt('Enter the desired size (max: 100)', '16');
 });
