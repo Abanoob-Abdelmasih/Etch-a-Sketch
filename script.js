f046b8c99c089370023b92f54033cbb6459c863c
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-02 14:31:25 +0300

    added change background color on hover

--- script.js
+++ script.js
@@ -2,7 +2,22 @@
 
 const container = document.querySelector('.container');
 
+function changeColor(e) {
+  console.log(e);
+  if (e.target.classList.contains('gridItem')) {
+    e.target.classList.add('hovered');
+  }
+}
+
 for (let i = 0; i < 256; i++) {
   const div = document.createElement('div');
+  div.classList.add('gridItem');
   container.appendChild(div);
 }
+
+const divs = document.querySelectorAll('div');
+console.log(divs);
+
+divs.forEach((div) => {
+  div.addEventListener('mouseenter', changeColor);
+});
