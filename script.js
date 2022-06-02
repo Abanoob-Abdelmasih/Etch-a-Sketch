3f2a5d4f4c97cbcd9044a082fa6bb0949a9b775a
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-02 13:54:49 +0300

    made the basic layout of the container and applied css to it

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-"use strict";
+'use strict';
+
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 256; i++) {
+  const div = document.createElement('div');
+  container.appendChild(div);
+}
